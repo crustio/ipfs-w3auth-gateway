7156bf74887f05afccbfec57ff1775ad491ab02c
Gavin Fu <gavfu@outlook.com>
2021-08-24 18:56:21 +0800

    Add proper error handling

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 require('dotenv').config();
 
+const _ = require('lodash');
+
 const http = require('http');
 const httpProxy = require('http-proxy');
 
@@ -18,30 +20,49 @@ const proxy = httpProxy.createProxyServer({});
 //
 const server = http.createServer(function(req: any, res: any) {
   // 1. Parse auth header as [pubKey, sig]
-  const authHeader = req.headers.authorization;
-  const authInfo = authHeader.substring("Basic ".length).trim();
-  
-  const buff = Buffer.from(authInfo, 'base64');
-  const unpw = buff.toString('ascii').split(':');
-  
-  const address = unpw[0];
-  const sig = unpw[1];
-  console.log(`Got pk(${address}) sig(${sig})`);
+  if (!_.includes(req.headers.authorization, 'Basic ')) {
+    res.writeHead(401, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({
+      message: 'Empty Signature'
+    }));
+    return;
+  }
 
-  // 2. Validate with substrate
-  // TODO: More web3 validating methods, like ethereum, solana, filecoin, ...
-  const publicKey = decodeAddress(address);
-  const hexPublicKey = u8aToHex(publicKey);
-  const isValid = signatureVerify(address, sig, hexPublicKey).isValid;
+  let isValid = false;
+  try {
+    const base64Credentials = _.split(_.trim(req.headers.authorization), ' ')[1];
+    const credentials = Buffer.from(base64Credentials, 'base64').toString('ascii');
+    const [address, sig] = credentials.split(':');
+    console.log(`Got public address '${address}' and sigature '${sig}'`);
+
+    // 2. Validate with substrate
+    // TODO: More web3 validating methods, like ethereum, solana, filecoin, ...
+    const publicKey = decodeAddress(address);
+    const hexPublicKey = u8aToHex(publicKey);
+    isValid = signatureVerify(address, sig, hexPublicKey).isValid;
+  }
+  catch (error) {
+    console.error(error.message);
+    res.writeHead(401, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({
+      message: 'Invalid Signature'
+    }));
+    return;
+  }
   
   // You can define here your custom logic to handle the request
   // and then proxy the request.
   if (isValid === true) {
     console.log('Validation success');
     proxy.web(req, res, { target: process.env.PROXY_TARGET || 'http://127.0.0.1:5001' });
-  } else {
+  }
+  else {
     console.error('Validation failed');
-    res.end();
+    res.writeHead(401, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({
+      data: 'Invalid Signature'
+    }));
+    return;
   }
 });
 
