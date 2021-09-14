bb7ea440a73a453ffb8af1f4e0704b27b3d2f88f
Gavin Fu <gavfu@outlook.com>
2021-09-14 15:38:28 +0800

    Support eth signature generated by mycrypto and myetherwallet

--- src/auth/ethAuth.ts
+++ src/auth/ethAuth.ts
@@ -1,19 +1,35 @@
 import {AuthData} from './types';
+import {ethers} from 'ethers';
 import * as _ from 'lodash';
 
-const EthAccounts = require('web3-eth-accounts');
-const ethAccounts = new EthAccounts();
+function addressesEquals(address: string, recoverAddress: string): boolean {
+  return _.toLower(_.trim(recoverAddress)) === _.toLower(_.trim(address));
+}
 
 function auth(data: AuthData): boolean {
   const {address, signature} = data;
 
   console.log('Validate as ethereum signature.');
-  const signatureWithPrefix = _.startsWith(signature, '0x')
+  const signatureWithPrefix = _.startsWith(_.toLower(signature), '0x')
     ? signature
     : `0x${signature}`;
-  const recoveredAddress = ethAccounts.recover(address, signatureWithPrefix);
-  console.log(`Recovered ethereum address ${recoveredAddress}`);
-  return _.toLower(_.trim(recoveredAddress)) === _.toLower(_.trim(address));
+
+  // For some signing tools like mycrypto, we can directly verify the signature
+  let recoveredAddress = ethers.utils.verifyMessage(
+    address,
+    signatureWithPrefix
+  );
+  if (addressesEquals(address, recoveredAddress)) {
+    return true;
+  }
+  // Some some signing tools like myetherwallet, we need hash the message before recover
+  const hashBytes = ethers.utils.arrayify(address);
+  const messageHash = ethers.utils.hashMessage(hashBytes);
+  recoveredAddress = ethers.utils.recoverAddress(
+    messageHash,
+    signatureWithPrefix
+  );
+  return addressesEquals(address, recoveredAddress);
 }
 
 export default {
