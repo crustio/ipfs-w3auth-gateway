fdd99f0d3e3ed112bcedd295cf21123d5e013de3
MyronFanQiu <49134743+MyronFanQiu@users.noreply.github.com>
2021-09-15 16:08:55 +0800

    Support near wallet (#16)
    * support near wallet
    
    * support nea as well

--- src/auth/authRegistry.ts
+++ src/auth/authRegistry.ts
@@ -10,9 +10,9 @@ const mapBySigType = (sigTypes: string[], authObject: object) => {
 };
 
 const authProviders = {
-  ...mapBySigType(['substrate', 'sub', 'cru'], SubstrateAuth),
-  ...mapBySigType(['ethereum', 'eth', 'pol'], EthAuth),
-  ...mapBySigType(['solana', 'sol'], SolanaAuth),
+  ...mapBySigType(['substrate', 'sub', `crust`, 'cru'], SubstrateAuth),
+  ...mapBySigType(['ethereum', 'eth', 'polygon', 'pol'], EthAuth),
+  ...mapBySigType(['solana', 'sol', 'near', `nea`], SolanaAuth),
 };
 
 function auth(signatureType: string, data: AuthData): boolean {
