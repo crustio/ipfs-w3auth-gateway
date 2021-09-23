379421b97a3d4f45325e4fefe9644cef52d67f57
MyronFanQiu <qiufan_glamour@hotmail.com>
2021-09-23 16:18:46 +0800

    use middlewares

--- src/index.ts
+++ src/index.ts
@@ -1,73 +1,21 @@
 /* eslint-disable node/no-extraneous-import */
 require('dotenv').config();
 
-import * as _ from 'lodash';
 import * as httpProxy from 'http-proxy';
 import {Request, Response} from 'express';
 
-import {AuthError} from './auth/types';
-import authRegistry from './auth/authRegistry';
+import * as authHandler from '@crustio/auth-handler';
 
 const express = require('express');
 const cors = require('cors');
 
 const server = express();
 server.use(cors());
+server.use(authHandler);
 
 const proxy = httpProxy.createProxyServer({});
 
-const chainTypeDelimiter = '-';
-const pkSigDelimiter = ':';
-
 server.all('*', (req: Request, res: Response) => {
-  // 1. Parse basic auth header 'Authorization: Basic [AuthToken]'
-  if (!_.includes(req.headers.authorization, 'Basic ')) {
-    res.writeHead(401, {'Content-Type': 'application/json'});
-    res.end(
-      JSON.stringify({
-        Error: 'Empty Signature',
-      })
-    );
-    return;
-  }
-
-  let isValid = false;
-  try {
-    // 2. Decode AuthToken
-    const base64Credentials = _.split(
-      _.trim(req.headers.authorization),
-      ' '
-    )[1];
-    const credentials = Buffer.from(base64Credentials, 'base64').toString(
-      'ascii'
-    );
-
-    // 3. Parse AuthToken as `ChainType[substrate/eth/solana].PubKey:SignedMsg`
-    const [passedAddress, sig] = _.split(credentials, pkSigDelimiter);
-    console.log(`Got public address '${passedAddress}' and sigature '${sig}'`);
-
-    // 4. Extract chain type, default: 'sub' if not specified
-    const gaugedAddress = _.includes(passedAddress, chainTypeDelimiter)
-      ? passedAddress
-      : `sub${chainTypeDelimiter}${passedAddress}`;
-    const [chainType, address] = _.split(gaugedAddress, chainTypeDelimiter);
-
-    isValid = authRegistry.auth(chainType, {
-      address,
-      signature: sig,
-    });
-  } catch (error) {
-    console.error(error.message);
-    res.writeHead(401, {'Content-Type': 'application/json'});
-    res.end(
-      JSON.stringify({
-        Error: error instanceof AuthError ? error.message : 'Invalid Signature',
-      })
-    );
-    return;
-  }
-
-  if (isValid === true) {
     const target = process.env.IPFS_ENDPOINT || 'http://127.0.0.1:5001';
     console.log(`Validation success. Proxying request to ${target}`);
 
@@ -80,17 +28,8 @@ server.all('*', (req: Request, res: Response) => {
         })
       );
     });
-  } else {
-    console.error('Validation failed');
-    res.writeHead(401, {'Content-Type': 'application/json'});
-
-    res.end(
-      JSON.stringify({
-        Error: 'Invalid Signature',
-      })
-    );
   }
-});
+);
 
 const port = process.env.PORT || 5050;
 console.log(`Listening on port ${port}`);
