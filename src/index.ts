436f8024da95dbccb65f6a325cee2e9c8a8de4f2
MyronFanQiu <qiufan_glamour@hotmail.com>
2021-09-28 10:52:01 +0800

    fix lint

--- src/index.ts
+++ src/index.ts
@@ -16,20 +16,19 @@ server.use(authHandler);
 const proxy = httpProxy.createProxyServer({});
 
 server.all('*', (req: Request, res: Response) => {
-    const target = process.env.IPFS_ENDPOINT || 'http://127.0.0.1:5001';
-    console.log(`Validation success. Proxying request to ${target}`);
+  const target = process.env.IPFS_ENDPOINT || 'http://127.0.0.1:5001';
+  console.log(`Validation success. Proxying request to ${target}`);
 
-    proxy.web(req, res, {target}, error => {
-      console.error(error);
-      res.writeHead(500, {'Content-Type': 'application/json'});
-      res.end(
-        JSON.stringify({
-          Error: error.message,
-        })
-      );
-    });
-  }
-);
+  proxy.web(req, res, {target}, error => {
+    console.error(error);
+    res.writeHead(500, {'Content-Type': 'application/json'});
+    res.end(
+      JSON.stringify({
+        Error: error.message,
+      })
+    );
+  });
+});
 
 const port = process.env.PORT || 5050;
 console.log(`Listening on port ${port}`);
