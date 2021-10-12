b9b354837e9ecc1a190fd89a0491ffa63d3b77bd
kun <zikunvan@gmail.com>
2021-10-12 11:43:12 +0800

    Update dockerize.sh

--- dockerize.sh
+++ dockerize.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-PACKAGE_VERSION=$(cat package.json \
-  | grep version \
-  | head -1 \
-  | awk -F: '{ print $2 }' \
-  | sed 's/[",]//g' \
-  | tr -d '[[:space:]]')
-IMAGEID="crustio/ipfs-w3auth:$PACKAGE_VERSION"
-echo "Building crustio/ipfs-w3auth:$PACKAGE_VERSION ..."
+IMAGEID="crustio/ipfs-w3auth:latest"
+echo "Building crustio/ipfs-w3auth:latest ..."
 docker build -t $IMAGEID .
